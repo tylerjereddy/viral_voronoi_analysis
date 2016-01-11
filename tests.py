ed48f1840b9681d7404d4901a0b183f217b50ab4
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-01-11 14:54:39 +0000

    Added new unit tests for Voronoi neighbour counting (currently fail--will be important to resolve these). Also, improved the setUp method of the test class.

--- tests.py
+++ tests.py
@@ -8,9 +8,9 @@ class TestRawNeighbourAnalysis(unittest.TestCase):
     def setUp(self):
         self.small_data_dict = {'POPC':{},'PPCE':{}} #build a dict with simple coords to test neighbour properties
         self.template_array = np.zeros((3,3))
-        self.a = self.template_array[:]
-        self.b = self.template_array[:]
-        self.c = self.template_array[:]
+        self.a = np.copy(self.template_array)
+        self.b = np.copy(self.template_array)
+        self.c = np.copy(self.template_array)
         self.a[0,...] = np.ones((1,3))
         self.b[1,...] = np.ones((1,3))
         self.c[2,...] = np.ones((1,3))
@@ -52,3 +52,44 @@ class TestRawNeighbourAnalysis(unittest.TestCase):
         self.assertEqual(outer_leaflet_neighbour_dict['PPCE'].keys(), [0], "Outer leaflet PPCE should have zero neighbours.") 
         self.assertEqual(outer_leaflet_neighbour_dict['PPCE'][0]['frequency'], 3, "Outer leaflet PPCE should have 3 cells (a,b,c) with 0 neighbours.")
         self.assertEqual(inner_leaflet_neighbour_dict['POPC'][0]['frequency'], 3, "Inner leaflet POPC should have 3 cells (a,b,c) with 0 neighbours.")
+
+    def test_neighbour_counting_single_unique_cell(self):
+        '''Using artificial polygon (a) as a single unique input, with all other artificial polygons containing only 
+        zeros, a neighbour counting result should indicate that a has 4 neighbours (2 from each residue type) and that each of those 4 neighbours has only
+        2 neighbours each (1 copy of a from each residue), given two residues with three Voronoi cells each.'''
+        self.a[1,...] = np.ones((1,3)) + 5.
+        self.a[2,...] = np.ones((1,3)) + 9.
+        for resname, subdict in self.small_data_dict.iteritems():
+            subdict['voronoi_cell_list_vertex_arrays'] = [[self.a,self.template_array,self.template_array]]
+            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[self.a,self.template_array,self.template_array]]
+        voronoi_neighbour_instance = voronoi_analysis_library.voronoi_neighbour_analysis(self.small_data_dict, inner_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays_inner_leaflet',outer_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays')
+        inner_leaflet_neighbour_dict, outer_leaflet_neighbour_dict = voronoi_neighbour_instance.identify_voronoi_neighbours(frame_index = 0)
+        raise Exception('inner_leaflet_neighbour_dict: {inner_dict} {a} {b}'.format(inner_dict=inner_leaflet_neighbour_dict, a=self.a, b=self.template_array))
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Inner leaflet POPC should have either 2 or 4 neighbours, but got {result}.".format(result=inner_leaflet_neighbour_dict['POPC'].keys())) 
+        self.assertEqual(inner_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Inner leaflet PPCE should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Outer leaflet POPC should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Outer leaflet PPCE should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'][2]['frequency'], 2, "Outer leaflet PPCE should have 2 cells with 2 neighbours.")
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'][2]['frequency'], 2, "Inner leaflet POPC should have 2 cells with 2 neighbours.")
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'][4]['frequency'], 1, "Outer leaflet PPCE should have 1 cell with 4 neighbours.")
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'][4]['frequency'], 1, "Inner leaflet POPC should have 1 cell with 4 neighbours.")
+
+    def test_optimized_neighbour_counting_single_unique_cell(self):
+        '''Using artificial polygon (a) as a single unique input, with all other artificial polygons containing only 
+        zeros, a neighbour counting result should indicate that a has 4 neighbours (2 from each residue type) and that each of those 4 neighbours has only
+        2 neighbours each (1 copy of a from each residue), given two residues with three Voronoi cells each.'''
+        #self.a[1,...] = np.ones((1,3)) + 5.
+        for resname, subdict in self.small_data_dict.iteritems():
+            subdict['voronoi_cell_list_vertex_arrays'] = [[self.a,self.template_array,self.template_array]]
+            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[self.a,self.template_array,self.template_array]]
+        voronoi_neighbour_instance = voronoi_analysis_library.voronoi_neighbour_analysis_optimized(self.small_data_dict, inner_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays_inner_leaflet',outer_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays')
+        inner_leaflet_neighbour_dict, outer_leaflet_neighbour_dict = voronoi_neighbour_instance.identify_voronoi_neighbours(frame_index = 0)
+        raise Exception('inner_leaflet_neighbour_dict: {inner_dict}'.format(inner_dict=inner_leaflet_neighbour_dict))
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Inner leaflet POPC should have either 2 or 4 neighbours, but got {result} {a} {b}.".format(result=inner_leaflet_neighbour_dict['POPC'].keys(), a = self.a, b = self.template_array)) 
+        self.assertEqual(inner_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Inner leaflet PPCE should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Outer leaflet POPC should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Outer leaflet PPCE should have either 2 or 4 neighbours.") 
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'][2]['frequency'], 2, "Outer leaflet PPCE should have 2 cells with 2 neighbours.")
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'][2]['frequency'], 2, "Inner leaflet POPC should have 2 cells with 2 neighbours.")
+        self.assertEqual(outer_leaflet_neighbour_dict['PPCE'][4]['frequency'], 1, "Outer leaflet PPCE should have 1 cell with 4 neighbours.")
+        self.assertEqual(inner_leaflet_neighbour_dict['POPC'][4]['frequency'], 1, "Inner leaflet POPC should have 1 cell with 4 neighbours.")
