463d3fe1d5279c69f8e38689dbc7f358a619b69c
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-01-18 11:19:26 +0000

    Raw neighbour counting unit tests that involve a single unique cell now have slightly more realistic input data. This allows the tests to pass.

--- tests.py
+++ tests.py
@@ -55,16 +55,15 @@ class TestRawNeighbourAnalysis(unittest.TestCase):
 
     def test_neighbour_counting_single_unique_cell(self):
         '''Using artificial polygon (a) as a single unique input, with all other artificial polygons containing only 
-        zeros, a neighbour counting result should indicate that a has 4 neighbours (2 from each residue type) and that each of those 4 neighbours has only
-        2 neighbours each (1 copy of a from each residue), given two residues with three Voronoi cells each.'''
-        self.a[1,...] = np.ones((1,3)) + 5.
-        self.a[2,...] = np.ones((1,3)) + 9.
+        one matching vertex to a, a neighbour counting result should indicate that (a) has 4 neighbours (2 from each residue type) and that each of those 4 neighbours has only
+        2 neighbours each (1 copy of (a) from each residue), given two residues with three Voronoi cells each.'''
+        a = np.array([[3,3,3],[5,5,5],[7,7,7]])
+        b = np.array([[3,3,3],[0,0,0],[9,9,9]])
         for resname, subdict in self.small_data_dict.iteritems():
-            subdict['voronoi_cell_list_vertex_arrays'] = [[self.a,self.template_array,self.template_array]]
-            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[self.a,self.template_array,self.template_array]]
+            subdict['voronoi_cell_list_vertex_arrays'] = [[a,b,b]]
+            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[a,b,b]]
         voronoi_neighbour_instance = voronoi_analysis_library.voronoi_neighbour_analysis(self.small_data_dict, inner_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays_inner_leaflet',outer_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays')
         inner_leaflet_neighbour_dict, outer_leaflet_neighbour_dict = voronoi_neighbour_instance.identify_voronoi_neighbours(frame_index = 0)
-        raise Exception('inner_leaflet_neighbour_dict: {inner_dict} {a} {b}'.format(inner_dict=inner_leaflet_neighbour_dict, a=self.a, b=self.template_array))
         self.assertEqual(inner_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Inner leaflet POPC should have either 2 or 4 neighbours, but got {result}.".format(result=inner_leaflet_neighbour_dict['POPC'].keys())) 
         self.assertEqual(inner_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Inner leaflet PPCE should have either 2 or 4 neighbours.") 
         self.assertEqual(outer_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Outer leaflet POPC should have either 2 or 4 neighbours.") 
@@ -75,16 +74,14 @@ class TestRawNeighbourAnalysis(unittest.TestCase):
         self.assertEqual(inner_leaflet_neighbour_dict['POPC'][4]['frequency'], 1, "Inner leaflet POPC should have 1 cell with 4 neighbours.")
 
     def test_optimized_neighbour_counting_single_unique_cell(self):
-        '''Using artificial polygon (a) as a single unique input, with all other artificial polygons containing only 
-        zeros, a neighbour counting result should indicate that a has 4 neighbours (2 from each residue type) and that each of those 4 neighbours has only
-        2 neighbours each (1 copy of a from each residue), given two residues with three Voronoi cells each.'''
-        #self.a[1,...] = np.ones((1,3)) + 5.
+        '''Similar to non-optimized version of test above.'''
+        a = np.array([[3,3,3],[5,5,5],[7,7,7]])
+        b = np.array([[3,3,3],[0,0,0],[9,9,9]])
         for resname, subdict in self.small_data_dict.iteritems():
-            subdict['voronoi_cell_list_vertex_arrays'] = [[self.a,self.template_array,self.template_array]]
-            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[self.a,self.template_array,self.template_array]]
+            subdict['voronoi_cell_list_vertex_arrays'] = [[a,b,b]]
+            subdict['voronoi_cell_list_vertex_arrays_inner_leaflet'] = [[a,b,b]]
         voronoi_neighbour_instance = voronoi_analysis_library.voronoi_neighbour_analysis_optimized(self.small_data_dict, inner_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays_inner_leaflet',outer_leaflet_vertex_list_key = 'voronoi_cell_list_vertex_arrays')
         inner_leaflet_neighbour_dict, outer_leaflet_neighbour_dict = voronoi_neighbour_instance.identify_voronoi_neighbours(frame_index = 0)
-        raise Exception('inner_leaflet_neighbour_dict: {inner_dict}'.format(inner_dict=inner_leaflet_neighbour_dict))
         self.assertEqual(inner_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Inner leaflet POPC should have either 2 or 4 neighbours, but got {result} {a} {b}.".format(result=inner_leaflet_neighbour_dict['POPC'].keys(), a = self.a, b = self.template_array)) 
         self.assertEqual(inner_leaflet_neighbour_dict['PPCE'].keys(), [2,4], "Inner leaflet PPCE should have either 2 or 4 neighbours.") 
         self.assertEqual(outer_leaflet_neighbour_dict['POPC'].keys(), [2,4], "Outer leaflet POPC should have either 2 or 4 neighbours.") 
